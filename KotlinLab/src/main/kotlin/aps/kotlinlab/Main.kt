14b9e93bacd06a01b5ec2279fa766e0e1775bd82
André <4ndpsilva@gmail.com>
2020-09-21 23:39:11 -0300

    Testes com range

--- KotlinLab/src/main/kotlin/aps/kotlinlab/Main.kt
+++ KotlinLab/src/main/kotlin/aps/kotlinlab/Main.kt
@@ -1,4 +1,12 @@
 package aps.kotlinlab
 
-class Main {
+import java.time.LocalDate
+
+fun main(args: Array<String>){
+    val start = LocalDate.of(2020, 1, 1)
+    val end = LocalDate.of(2020, 1, 10)
+
+    val range = start..end
+    val date = LocalDate.of(2020, 11, 5)
+    println(date !in range)
 }
